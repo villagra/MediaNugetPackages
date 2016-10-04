26383093ede3e33ceaf23d4d1c018bd46ce7469a
villagra <villagra@gmail.com>
2016-10-04 16:06:08 +0200

    Added Player Framework Packages

---
+++ packageAll.bat
@@ -0,0 +1,7 @@
+
+nuget pack Microsoft.Media.Advertising\Package.nuspec
+nuget pack Microsoft.Media.TimedText\Package.nuspec
+nuget pack Microsoft.PlayerFramework.Advertising\Package.nuspec
+nuget pack Microsoft.PlayerFramework.Core\Package.nuspec
+nuget pack Microsoft.PlayerFramework.TimedText\Package.nuspec
+
